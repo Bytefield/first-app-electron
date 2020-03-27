384dbee7d494ae94158fdd82388c5b9f8c2e9fd8
Bytefield <ramosperez1978@gmail.com>
2020-03-27 23:41:41 +0100

    added additem

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require('path')
 const { app, BrowserWindow, Menu } = electron
 
 let mainWindow
+let addWindow
 
 // Listen for app to be ready
 app.on('ready', function() {
@@ -25,13 +26,32 @@ app.on('ready', function() {
 
 })
 
+// Handle create add window
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 200,
+        height: 300,
+        title: 'Add Shopping List Item'
+    })
+
+    //Load html file into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname + 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+}
+
 // Create menu template
 const mainMenuTemplate = [
     {
         label: 'File',
         submenu: [
             {
-                label: 'Add Item'
+                label: 'Add Item',
+                click() {
+
+                }
             },
             {
                 label: 'Clear Items'
